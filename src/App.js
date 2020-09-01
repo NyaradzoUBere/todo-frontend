512bce633faf6f04db20734c4976980204c35442
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-31 22:25:19 -0600

    set state using fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-
+import TodoContainer from './components/TodoContainer'
 import './App.css';
+
+const todosURL = "http://localhost:3000/todos"
 
 class App extends React.Component {
   state = {
     todos: []
   }
+
+  componentDidMount() {
+    this.getTodos()
+  }
+
+  getTodos = () => {
+    fetch(todosURL)
+      .then(response => response.json())
+      .then(todos => this.setState({todos}))
+  }
   render() {
     return (
       <div className="App">
         <h1>Todo App</h1>
+        <TodoContainer todos = {this.state.todos} />
       </div>
     );
   }
